fe24d811ebc7a3616e75e2326a5cc33fdb9d1bd3
ZJMIIT <565044566@qq.com>
2020-08-12 15:38:19 +0800

    add regist operate and logon layout and somthing about logon logic verification

--- app/src/main/java/com/example/myapp/RegistActivity.java
+++ app/src/main/java/com/example/myapp/RegistActivity.java
@@ -2,9 +2,14 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegistActivity extends AppCompatActivity {
@@ -17,14 +22,49 @@ public class RegistActivity extends AppCompatActivity {
     }
 
     public void regist(View view){//注册操作
+        EditText editText_account=findViewById(R.id.regist_editText_account);
+        EditText editText_pwd=findViewById(R.id.regist_editText_password);
+        EditText editText_pwd2=findViewById(R.id.regist_editText_password2);
+        String name=editText_account.getText().toString();
+        String pwd=editText_pwd.getText().toString();
+        String pwd2=editText_pwd2.getText().toString();
+        TextView alarm=findViewById(R.id.regist_textView_alarm);
+        alarm.setText("");//清空警示语
 
-        Toast.makeText(RegistActivity.this,"you click the regist button",Toast.LENGTH_LONG).show();
+        DBOpenHelper dbOpenHelper=new DBOpenHelper(this,"user.db",null,1);//调用dbopenhelp创建user表
+        SQLiteDatabase db=dbOpenHelper.getWritableDatabase();
+        Cursor c=db.query("user_tb",null,"userID=?",new String[]{name},null,null,null);//搜索用户名密码
+        if(c!=null&&c.getCount()>1){//判断用户名是否存在
+            alarm.setText("用户名存在");
+            c.close();//关闭
+        }else{
+            if(pwd.equals(pwd2)){
+                if(name.equals("")){
+                    alarm.setText("请输入用户名");
+                }else{
+                    ContentValues values=new ContentValues();
+                    values.put("userID",name);
+                    values.put("pwd",pwd);
+                    long rows=db.insert("user_tb",null,values);
+                    Intent intent=new Intent(this,LoginActivity.class);
+                    startActivity(intent);
+                    this.finish();
+                }
+            }else{
+                alarm.setText("两次密码不一致");
+            }
+        }
+        db.close();
     }
 
-    public void reset(View view){//清空文本编辑框中的信息
-        EditText editText_account=findViewById(R.id.login_editText_account);
-        EditText editText_password=findViewById(R.id.login_editText_password);
+    public void r_reset(View view){//清空文本编辑框中的信息
+        EditText editText_account=findViewById(R.id.regist_editText_account);
+        EditText editText_password=findViewById(R.id.regist_editText_password);
+        EditText editText_password2=findViewById(R.id.regist_editText_password2);
         editText_account.setText("");
         editText_password.setText("");
+        editText_password2.setText("");
+        TextView alarm=findViewById(R.id.regist_textView_alarm);
+        alarm.setText("");//清空警示语
     }
 }
