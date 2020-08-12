1b604203e6623e56a8daa26c693e13f537bbd5bb
ZJMIIT <565044566@qq.com>
2020-08-12 15:40:00 +0800

    add regist operate and logon layout and somthing about logon logic verification

--- app/src/main/java/com/example/myapp/LoginActivity.java
+++ app/src/main/java/com/example/myapp/LoginActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,21 +26,26 @@ public class LoginActivity extends AppCompatActivity {
         EditText editText_password=findViewById(R.id.login_editText_password);
         String name=editText_account.getText().toString();
         String pwd=editText_password.getText().toString();
+        TextView alarm=findViewById(R.id.login_textView_alarm);
+        alarm.setText("");//清空警示语
         DBOpenHelper dbOpenHelper=new DBOpenHelper(this,"user.db",null,1);//调用dbopenhelp创建user表
         SQLiteDatabase db=dbOpenHelper.getWritableDatabase();
         Cursor c=db.query("user_tb",null,"userID=? and pwd=?",new String[]{name,pwd},null,null,null);//搜索用户名密码
-        if(c!=null&&c.getCount()>1){
-//            String[] cols=c.getColumnNames();
-//            while (c.moveToNext()){
-//                for (String ColumName:cols){
-//                    Log.i("info",ColumName+":"+c.getString(c.getColumnIndex(ColumName)));
-//                }
-//            }
+        if(c!=null&&c.getCount()>=1){
+            String[] cols=c.getColumnNames();
+            while (c.moveToNext()){
+                for (String ColumName:cols){
+                    Log.i("info",ColumName+":"+c.getString(c.getColumnIndex(ColumName)));
+                }
+            }
             c.close();
             db.close();
+            Intent intent=new Intent(this,BodyActivity.class);
+            startActivity(intent);
+            this.onPause();
             this.finish();
         }else{
-            Toast.makeText(LoginActivity.this,"用户名或密码错误",Toast.LENGTH_LONG).show();
+            alarm.setText("用户名或密码错误");
         }
     }
 
@@ -49,10 +55,12 @@ public class LoginActivity extends AppCompatActivity {
         this.onPause();
     }
 
-    public void reset(View view){//清空文本编辑框中的信息
+    public void l_reset(View view){//清空文本编辑框中的信息
         EditText editText_account=findViewById(R.id.login_editText_account);
         EditText editText_password=findViewById(R.id.login_editText_password);
         editText_account.setText("");
         editText_password.setText("");
+        TextView alarm=findViewById(R.id.login_textView_alarm);
+        alarm.setText("");//清空警示语
     }
 }
